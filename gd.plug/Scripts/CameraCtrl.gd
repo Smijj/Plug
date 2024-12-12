8a126046e9323aca1f1860e0d6a83f2f7d68bf85
Declan Smyth (Home PC) <declan.smyth@hotmail.com>
2024-12-12 17:50:36 +0930

    Setup proper movement and playing the animations. Setup level and lighting. Created more assets.

---
+++ gd.plug/Scripts/CameraCtrl.gd
@@ -0,0 +1,24 @@
+extends Node3D
+
+@export_group("Camera")
+@export var _MouseSensitivity := 0.2
+@export var _CamerYPivot :Node3D
+@export var _CameraXPivot :Node3D
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event:InputEvent):
+	_HandleCamera(event)	# Handle Camera and player y rotation
+	
+func _HandleCamera(event:InputEvent):
+	if not event is InputEventMouseMotion: return
+	
+	# Rotate whole player around the y axis to look left and right
+	if _CamerYPivot:
+		_CamerYPivot.rotate_y(deg_to_rad(-event.relative.x * _MouseSensitivity))
+	
+	# Rotate just the camera around the x axis to look up and down
+	if _CameraXPivot:
+		_CameraXPivot.rotate_x(deg_to_rad(-event.relative.y * _MouseSensitivity))
+		_CameraXPivot.rotation.x = clamp(_CameraXPivot.rotation.x, deg_to_rad(-90), deg_to_rad(45))	# Clamp camera up/down motion
--- gd.plug/Scripts/PlayerCtrl.gd
+++ gd.plug/Scripts/PlayerCtrl.gd
@@ -1,11 +1,26 @@
 extends CharacterBody3D
 
 
-@export var SPEED = 5.0
-@export var JUMP_VELOCITY = 4.5
+@export var SPEED:float = 5.0
+@export var JUMP_VELOCITY:float = 4.5
+@export var RotationSmoothing:float = 5
 
+@export var _Anim: AnimationPlayer
+@export var _CharacterModel: Node3D
+
+var _OnGroundLastFrame: bool = true
+var _MovingLastFrame: bool = false
+var _Jumping: bool = false
+var _ModelLookPos: Vector3 = Vector3.ZERO
 
 func _physics_process(delta: float) -> void:
+	
+	# If you just landed on the and wen't on the ground last frame, play idle anim
+	if is_on_floor() and !_OnGroundLastFrame:
+		_Jumping = false
+		_Anim.play("Idle")
+	_OnGroundLastFrame = is_on_floor()
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -13,16 +28,29 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		_Jumping = true
+		_Anim.play("Jump")
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("Move_Left", "Move_Right", "Move_Forward", "Move_Backwards")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		#_CharacterModel.look_at(global_position+direction)
+		_ModelLookPos = _ModelLookPos.move_toward(global_position + direction, RotationSmoothing)
+		_CharacterModel.look_at(_ModelLookPos, Vector3(0,1,0), false)
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		_MovingLastFrame = true
+		
+		if !_Jumping:
+			_Anim.play("Move_Forward")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+		
+		# If you stop moving and are on teh floor, play idle
+		if is_on_floor() and _MovingLastFrame:
+			_Anim.play("Idle")
+		_MovingLastFrame = false
 
 	move_and_slide()
